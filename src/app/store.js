62ed283874d615e7ff20b3e5ff940a478eec7044
Hyun Don Moon <hyundonmoon@gmail.com>
2022-02-04 23:27:41 +0900

    initial commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,6 @@
+/* eslint-disable import/prefer-default-export */
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {},
 });
